a1f6435e3368b8941b449bc51868695795b0a41a
Pixdane <yuanjin233@gmail.com>
2025-03-30 19:31:21 +0800

    Updated ChiselableXtones.groovy

---
+++ groovy/postInit/mods/ChiselableXtones.groovy
@@ -0,0 +1,41 @@
+def types = [
+	"agon",
+	"azur",
+	"bitt",
+	"cray",
+	"fort",
+	"glaxx",
+	"iszm",
+	"jelt",
+	"korp",
+	"kryp",
+	"lair",
+	"lave",
+	"mint",
+	"myst",
+	"reds",
+	"reed",
+	"roen",
+	"sols",
+	"sync",
+	"tank",
+	"vect",
+	"vena",
+	"zane",
+	"zech",
+	"zest",
+	"zeta",
+	"zion",
+	"zkul",
+	"zoea",
+	"zome",
+	"zone",
+	"zorg",
+	"ztyl",
+	"zyth"
+] as String[];
+
+for (block in types) {
+	mods.chisel.Carving.addGroup(block);
+    mods.chisel.Carving.addVariation(block, item('xtones:' + block + ':*'));
+}
