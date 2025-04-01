cdd524859dcf38d96ddfe1cc39ccfa4528b76ef7
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-04-01 15:34:06 +0800

    feat: implement singleblock vial processors

--- groovy/classes/textures/TJTextures.groovy
+++ groovy/classes/textures/TJTextures.groovy
@@ -3,21 +3,9 @@ import gregtech.client.renderer.texture.cube.*
 
 class TJTextures {
 
-    // static SimpleOverlayRenderer DRACONIC_CASING
-    // static SimpleOverlayRenderer AWAKENED_DRACONIUM_CASING
-    // static SimpleOverlayRenderer REFINED_CHAOS_CASING
-    // static SimpleOverlayRenderer SOUL_INSULATION_CASING
-
     static final SimpleOverlayRenderer DRACONIC_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_draconic')
     static final SimpleOverlayRenderer AWAKENED_DRACONIUM_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_awakened_draconium')
     static final SimpleOverlayRenderer REFINED_CHAOS_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_refined_chaos')
     static final SimpleOverlayRenderer SOUL_INSULATION_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_soul_insulation')
-
-    // static void init() {
-    //     DRACONIC_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_draconic')
-    //     AWAKENED_DRACONIUM_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_awakened_draconium')
-    //     REFINED_CHAOS_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_refined_chaos')
-    //     SOUL_INSULATION_CASING = new SimpleOverlayRenderer('casings/multiblock/machine_casing_soul_insulation')
-    // }
 
 }
