6297f2463e7c64554f60b2d8a3fc42ce1d154407
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-04-04 19:01:23 +0800

    feat: hot iron chain

--- groovy/materials/MaterialModifications.groovy
+++ groovy/materials/MaterialModifications.groovy
@@ -8,12 +8,8 @@ class MaterialModifications {
     }
 
     static void changeFlags() {
-        //material('iron').addFlags("generate_small_gear)
-        //material('cosmic_neutronium').addFlags("generate_round")
-        //material('neutronium').addFlags("generate_round")
-        //material('qcd_confined_matter').addFlags("generate_gear")
-        //no_replication
 
+        //no_replication
         material('gcyl:adamantium').addFlags('no_replication')
         material('gcyl:vibranium').addFlags('no_replication')
         material('gcyl:taranium').addFlags('no_replication')
@@ -26,19 +22,17 @@ class MaterialModifications {
     }
 
     static void modifyProperties() {
+
+        material('iron').addBlastProperty(1751) // minimum temperature for hot ingot generation
+
+        // Ore
         material('nether_star').addOre(true)
-        //material('glowstone').addOre()
-        //material('cryotheum').addOre()
-        //material('pyrotheum').addOre()
         material('coke').addOre()
-        //material('plutonium_radioactive').addOre()
         material('neptunium').addOre()
         material('gcyl:rhodium_salt').addOre()
         material('californium').addOre()
         material('curium').addOre()
-        //material('plutonium').addOre()
         material('rutherfordium').addOre()
-        //material('duranium').addOre()
         material('tritanium').addOre()
         material('naquadria').addOre()
         material('bohrium').addOre()
@@ -49,9 +43,6 @@ class MaterialModifications {
         material('berkelium').addOre()
         material('rhodium').addOre()
         material('ruthenium').addOre()
-        //material('dubnium').addOre()
-        //material('titanium').addOre()
-        //material('tungsten').addOre()
 
         material('gcyl:lv_superconductor_base').getProperty(PropertyKey.WIRE).setLossPerBlock(6)
         material('gcyl:bright_steel').getProperty(PropertyKey.WIRE).setLossPerBlock(2)
--- groovy/recipes/byLine/HotIron.groovy
+++ groovy/recipes/byLine/HotIron.groovy
@@ -1,4 +1,42 @@
+import static classes.utils.TJUtility.tjId
+
 ALLOY_SMELTER = recipemap('alloy_smelter')
 
-// TODO
-// TODO: Hot Iron -> Iron 7.5 s 16 EU/t
+
+// Removes auto-generated recipes related to hot iron ingots
+// Hot Iron Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('dustIron'), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+
+// Hot Iron Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('dustIron'), metaitem('circuit.integrated').withNbt(['Configuration': 2])], [fluid('nitrogen') * 1000 * 1000])
+
+// Hot Iron Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('dustIronMagnetic'), metaitem('circuit.integrated').withNbt(['Configuration': 1])], null)
+
+// Hot Iron Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('dustIronMagnetic'), metaitem('circuit.integrated').withNbt(['Configuration': 2])], [fluid('nitrogen') * 1000 * 1000])
+
+// Iron Ingot * 1
+mods.gregtech.vacuum_freezer.removeByInput(120, [metaitem('ingotHotIron')], null)
+
+// Iron Ingot -> Hot Iron Ingot
+furnace.recipeBuilder()
+    .input(ore('ingotIron'))
+    .output(metaitem('ingotHotIron'))
+    .exp(0.1)
+    .register()
+
+// Hot Iron Ingot -> Wrought Iron Ingot
+crafting.shapelessBuilder()
+    .input(ore('craftingToolHardHammer'), ore('ingotHotIron'))
+    .name(tjId('wrought_iron_forging'))
+    .output(metaitem('ingotWroughtIron'))
+    .register()
+
+// Hot Iron Ingot -> Wrought Iron Ingot
+ALLOY_SMELTER.recipeBuilder()
+    .inputs(ore('ingotHotIron'))
+    .outputs(metaitem('ingotWroughtIron'))
+    .duration(150)
+    .EUt(6)
+    .buildAndRegister()
