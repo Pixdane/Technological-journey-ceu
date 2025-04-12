4747f77db6b76475483463ba9983d3bfa09a0182
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-04-12 13:43:05 +0800

    feat: add tooltips for cover items

--- groovy/classes/items/TJMetaItems.groovy
+++ groovy/classes/items/TJMetaItems.groovy
@@ -4,6 +4,8 @@ import gregtech.api.items.metaitem.StandardMetaItem
 import gregtech.common.covers.CoverConveyor
 import gregtech.common.covers.CoverPump
 import gregtech.common.covers.CoverSolarPanel
+import gregtech.common.items.behaviors.TooltipBehavior
+import net.minecraft.client.resources.I18n
 
 import static classes.utils.TJUtility.tjId
 import static gregtech.common.covers.CoverBehaviors.registerBehavior
@@ -38,10 +40,26 @@ class TJMetaItems {
         // Components 0 - 199
         STEAM_MOTOR_ULV = item.addItem(0, 'steam.motor.ulv')
         STEAM_PISTON_ULV = item.addItem(1, 'steam.piston.ulv')
+
         CONVEYOR_MODULE_ULV = item.addItem(2, 'conveyor.module.ulv')
+                .addComponents(new TooltipBehavior(lines -> {
+                    lines.add(I18n.format("metaitem.conveyor.module.tooltip"))
+                    lines.add(I18n.format("gregtech.universal.tooltip.item_transfer_rate", 4))
+                }))
+
         STEAM_PUMP_LV = item.addItem(3, 'steam.pump.ulv')
+                .addComponents(new TooltipBehavior({ lines ->
+                    lines.add(I18n.format("metaitem.electric.pump.tooltip"))
+                    lines.add(I18n.format("gregtech.universal.tooltip.fluid_transfer_rate", 32))
+                }))
 
         COVER_SOLAR_PANEL_MAX = item.addItem(4, 'solar_panel.max')
+                .addComponents(new TooltipBehavior({ lines ->
+                    lines.add(I18n.format("metaitem.cover.solar.panel.tooltip.1"))
+                    lines.add(I18n.format("metaitem.cover.solar.panel.tooltip.2"))
+                    lines.add(I18n.format("gregtech.universal.tooltip.voltage_out",
+                            GTValues.V[GTValues.MAX], GTValues.VNF[GTValues.MAX]))
+                }))
 
         // Ingredients 200 - 799
         REFINED_CHAOS_SHARD = item.addItem(200, 'shard.refined_chaos')
@@ -54,7 +72,7 @@ class TJMetaItems {
     }
 
     static void registerBehaviors() {
-        registerBehavior(tjId('conveyor.module.ulv'), CONVEYOR_MODULE_ULV, { defi, tile, side -> new CoverConveyor(defi, tile, side, GTValues.ULV, 640) })
+        registerBehavior(tjId('conveyor.module.ulv'), CONVEYOR_MODULE_ULV, { defi, tile, side -> new CoverConveyor(defi, tile, side, GTValues.ULV, 4) })
         registerBehavior(tjId('steam.pump.ulv'), STEAM_PUMP_LV, { defi, tile, side -> new CoverPump(defi, tile, side, GTValues.ULV, 640) })
 
         registerBehavior(tjId('solar_panel.max'), COVER_SOLAR_PANEL_MAX, { defi, tile, side -> new CoverSolarPanel(defi, tile, side, GTValues.V[GTValues.MAX]) })
