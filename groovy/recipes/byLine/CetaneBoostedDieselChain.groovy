298c8f4ca48f1a9fc3b365652c3128079668bb5f
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2025-04-12 16:03:26 +0900

    fix: conflicted commits

--- groovy/recipes/byLine/CetaneBoostedDieselChain.groovy
+++ groovy/recipes/byLine/CetaneBoostedDieselChain.groovy
@@ -1,45 +1,4 @@
 //TODO: from OtherRecipes
-
-<<<<<<< Updated upstream
-// Utils.removeRecipeByOutput(mixer, [], [fluid('nitro_fuel') * 1000], true)
-
-// MIXER.recipeBuilder()
-//         .fluidInputs([fluid('fuel') * 2000, fluid('tetranitromethane') * 20])
-//         .fluidOutputs([fluid('nitro_fuel') * 2000])
-//         .EUt(480)
-//         .duration(20)
-//         .buildAndRegister()
-
-// Utils.removeRecipeByOutput(largeMix, [], [fluid('nitro_fuel') * 1000], true)
-
-// LARGE_MIXER.recipeBuilder()
-//         .fluidInputs([fluid('fuel') * 2000, fluid('tetranitromethane') * 20])
-//         .fluidOutputs([fluid('nitro_fuel') * 2000])
-//         .circuitMeta(2)
-//         .EUt(480)
-//         .duration(20)
-//         .buildAndRegister()
-
-// //Nitro Benzeene
-
-// Utils.removeRecipeByOutput(chemreactor, [], [fluid('nitro_benzene') * 1000], true)
-
-// CHEMICAL_REACTOR.recipeBuilder()
-//         .fluidInputs(fluid('nitration_mixture') * 2000, fluid('benzene') * 5000, fluid('distilled_water') * 2000)
-//         .fluidOutputs(fluid('nitro_benzene') * 8000, fluid('diluted_sulfuric_acid') * 1000)
-//         .EUt(320)
-//         .duration(120)
-//         .buildAndRegister()
-
-// Utils.removeRecipeByOutput(large_chem, [], [fluid('nitro_benzene') * 1000], true)
-
-// LCR.recipeBuilder()
-//         .fluidInputs(fluid('nitration_mixture') * 2000, fluid('benzene') * 5000, fluid('distilled_water') * 2000)
-//         .fluidOutputs(fluid('nitro_benzene') * 8000, fluid('diluted_sulfuric_acid') * 1000)
-//         .EUt(320)
-//         .duration(120)
-//         .buildAndRegister()
-=======
 mods.gregtech.mixer.removeByInput(480, null, [fluid('diesel') * 1000 * 1000, fluid('tetranitromethane') * 20 * 20])
 
 MIXER = recipemap('mixer')
@@ -63,4 +22,3 @@ CHEMICAL_REACTOR.recipeBuilder()
         .EUt(320)
         .duration(120)
         .buildAndRegister()
->>>>>>> Stashed changes
--- groovy/recipes/byLine/PetrochemChain.groovy
+++ groovy/recipes/byLine/PetrochemChain.groovy
@@ -1,38 +1,3 @@
-<<<<<<< Updated upstream
-//TODO: from OtherRecipes
-
-// Utils.removeRecipeByOutput(tower, [], [fluid('sulfuric_heavy_fuel')], false) //triple all values-
-// DISTILLATION_TOWER.recipeBuilder()
-//         .fluidInputs(fluid('oil_heavy') * 4500)
-//         .fluidOutputs(fluid('sulfuric_heavy_fuel') * 7500, fluid('sulfuric_light_fuel') * 1350, fluid('sulfuric_naphtha') * 450, fluid('sulfuric_gas') * 18000)
-//         .duration(400)
-//         .EUt(10)
-//         .buildAndRegister()
-// DISTILLATION_TOWER.recipeBuilder()
-//         .fluidInputs(fluid('oil_medium') * 3000)
-//         .fluidOutputs(fluid('sulfuric_heavy_fuel') * 450, fluid('sulfuric_light_fuel') * 1500, fluid('sulfuric_naphtha') * 600, fluid('sulfuric_gas') * 1800)
-//         .duration(400)
-//         .EUt(10)
-//         .buildAndRegister()
-// DISTILLATION_TOWER.recipeBuilder()
-//         .fluidInputs(fluid('oil_light') * 450)
-//         .fluidOutputs(fluid('sulfuric_heavy_fuel') * 300, fluid('sulfuric_light_fuel') * 600, fluid('sulfuric_naphtha') * 900, fluid('sulfuric_gas') * 7500)
-//         .duration(400)
-//         .EUt(10)
-//         .buildAndRegister()
-// DISTILLATION_TOWER.recipeBuilder()
-//         .fluidInputs(fluid('oil') * 1500)
-//         .fluidOutputs(fluid('sulfuric_heavy_fuel') * 450, fluid('sulfuric_light_fuel') * 1500, fluid('sulfuric_naphtha') * 600, fluid('sulfuric_gas') * 1800)
-//         .duration(400)
-//         .EUt(10)
-//         .buildAndRegister()
-
-
-// Utils.removeRecipeByOutput(centrifuge, [], [fluid('oil') * 100], true)
-// Utils.removeRecipeByOutput(large_centrifuge, [], [fluid('oil') * 100], true)
-
-// distillery.findRecipe(24, [item('gregtech:meta_item_1:32766').withNbt({ Configuration: 2 })], [fluid('oil_light') * 150]).remove()
-=======
 mods.gregtech.distillation_tower.removeByInput(96, null, [fluid('oil') * 50 * 50])
 mods.gregtech.distillation_tower.removeByInput(96, null, [fluid('oil_light') * 150 * 150])
 mods.gregtech.distillation_tower.removeByInput(288, null, [fluid('oil_heavy') * 100 * 100])
@@ -69,4 +34,3 @@ DISTILLATION_TOWER.recipeBuilder()
 mods.gregtech.centrifuge.removeByInput(80, [item('minecraft:soul_sand')], null)
 
 mods.gregtech.distillery.removeByInput(24, [metaitem('circuit.integrated').withNbt(['Configuration': 2])], [fluid('oil_light') * 150 * 150])
->>>>>>> Stashed changes
