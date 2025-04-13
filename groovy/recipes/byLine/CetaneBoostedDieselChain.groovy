63bafa7287bc541dc2a89b6ed87f781221104a8a
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2025-04-13 10:10:34 +0900

    feat: nitro fuel recipe impl

--- groovy/recipes/byLine/CetaneBoostedDieselChain.groovy
+++ groovy/recipes/byLine/CetaneBoostedDieselChain.groovy
@@ -1,19 +1,17 @@
-//TODO: from OtherRecipes
-mods.gregtech.mixer.removeByInput(480, null, [fluid('diesel') * 1000 * 1000, fluid('tetranitromethane') * 20])
-
 MIXER = recipemap('mixer')
 CHEMICAL_REACTOR = recipemap('chemical_reactor')
 
-/*
+//Nitro Fuel
+mods.gregtech.mixer.removeByInput(480, null, [fluid('diesel') * 1000 * 1000, fluid('tetranitromethane') * 20])
+
 MIXER.recipeBuilder()
-        .fluidInputs([fluid('fuel') * 2000, fluid('tetranitromethane') * 20])
+        .fluidInputs([fluid('diesel') * 2000, fluid('tetranitromethane') * 20])
         .fluidOutputs([fluid('nitro_fuel') * 2000])
         .EUt(480)
         .duration(20)
         .buildAndRegister()
-*/
-//Nitro Benzeene
 
+//Nitro Benzeene
 mods.gregtech.chemical_reactor.removeByInput(480, null, [fluid('benzene') * 5000, fluid('nitration_mixture') * 2000, fluid('distilled_water') * 2000])
 
 CHEMICAL_REACTOR.recipeBuilder()
