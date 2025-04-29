0646e8157b0a48a2574c3383c652cde1cf170c05
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-04-29 22:51:16 +0800

    feat: dim display

---
+++ groovy/postInit/DimDisplay.groovy
@@ -0,0 +1,15 @@
+import tj.util.DimDisplayRegistry
+
+DimDisplayRegistry.set([
+        0   : metaitem('tjceu:display.overworld'),
+        (-1): metaitem('tjceu:display.nether'),
+        1   : metaitem('tjceu:display.end'),
+        3   : metaitem('tjceu:display.moon'),
+        400 : metaitem('tjceu:display.titan'),
+        401 : metaitem('tjceu:display.pluto'),
+        402 : metaitem('tjceu:display.phobos'),
+        403 : metaitem('tjceu:display.mercury'),
+        404 : metaitem('tjceu:display.deep_dark'),
+        405 : metaitem('tjceu:display.mars'),
+        406 : metaitem('tjceu:display.deimos'),
+])
